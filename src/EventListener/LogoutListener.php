94325e4f76fd9e24b62638b9b20f5640ca24c16c
John Fraser <burton_fsr@icloud.com>
2022-05-06 13:56:10 +0200

    Event Listener

--- src/EventListener/LogoutListener.php
+++ src/EventListener/LogoutListener.php
@@ -1,37 +1,30 @@
 <?php
 namespace App\EventListener;
- 
-use Doctrine\ORM\EntityManagerInterface;
+
+
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Http\Event\LogoutEvent;
- 
+
+
+
+
+
+
 class LogoutListener
 {
-    protected $entityManager;
- 
-    public function __construct(TokenStorageInterface $tokenStorage, EntityManagerInterface $entityManager)
+   
+
+    public function __construct(TokenStorageInterface $tokenStorage)
     {
         $this->tokenStorage = $tokenStorage;
-        $this->entityManager = $entityManager;
     }
- 
-    public function logout(LogoutEvent $logoutEvent)
+
+   
+    public function onKernelController()
     {
-        $token = $this->tokenStorage->getToken();
- 
-        $user = $token->getUser();
- 
+        $user = $this->tokenStorage->getToken()->getUser();
+
+        dd($user);
         
  
-        if ($user) {
-            $user->setSessionId('coucou');
-        }
- 
-       
- 
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
- 
-        $logoutEvent->getResponse();
     }
 }
